32a8273a9b514d4b35ef29231718ec5a673ac610
Wolle <schreibfaul1@t-online.de>
2018-03-16 00:14:59 +0100

    comment line 7 to 13
    new partitiontable default.cvs see additional_info, is now 16K

--- MiniWebradio/MiniWebRadio.ino
+++ MiniWebradio/MiniWebRadio.ino
@@ -4,13 +4,13 @@
 //
 //  if not enough space in nvs: change defaut.cvs
 //
-//  Name,     Type, SubType, Offset,   Size,     Flags
-//  otadata,  data, ota,     0xe000,   0x2000,
-//  app0,     app,  ota_0,   0x10000,  0x140000,
-//  app1,     app,  ota_1,   0x150000, 0x130000,
-//  nvs,      data, nvs,     0x280000, 0x10000,
-//  eeprom,   data, 0x99,    0x290000, 0x1000,
-//  spiffs,   data, spiffs,  0x291000, 0x169000
+//  Name,     Type,   SubType,   Offset,   Size,     Flags
+//  otadata,  data,   ota,       0x9000,   0x2000
+//  phy_init, data,   phy,       0xb000,   0x5000
+//  factory,  app,    factory,   0x10000,  1M
+//  ota_0,    app,    ota_0,     ,         1M
+//  ota_1,    app,    ota_1,     ,         1M
+//  nvs,      data,   nvs,       ,         0x16000
 //
 //  for german umlaut change Properties in Eclipse in UTF-8
 //  File/Properties/Resource:  Text file encoding (other) UTF-8
@@ -99,7 +99,7 @@ void defaultsettings(){
     pref.clear();
     pref.putString("ESP32_Radio","default");
     //
-    pref.putString("wifi_00","Wolles-FRITZBOX/xxxxx");
+    pref.putString("wifi_00","Wolles-FRITZBOX/40441061073895958449");
     pref.putString("wifi_01","ADSL-11/yyyyyy");
     //
     pref.putUInt("brightness",100); // 100% display backlight
