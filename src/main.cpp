a2fda367d43c2fc3647fb8011f11ac81b5fb8224
schreibfaul1 <schreibfaul1@t-online.de>
2022-04-10 21:14:53 +0200

    mute in websvr.reply mixed up

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
     MiniWebRadio -- Webradio receiver for ESP32
 
     first release on 03/2017
-    Version 2.2j, Apr 10/2022
+    Version 2.2k, Apr 10/2022
 
     2.8" color display (320x240px) with controller ILI9341 or HX8347D (SPI) or
     3.5" color display (480x320px) wiht controller ILI9486 or ILI9488 (SPI)
@@ -2041,9 +2041,9 @@ void WEBSRV_onCommand(const String cmd, const String param, const String arg){  
     if(cmd == "getstreamtitle"){    webSrv.reply(_streamTitle.c_str());
                                     return;}
 
-    if(cmd == "mute"){              mute();
-                                    if(_f_mute) webSrv.reply("Mute on\n");
-                                    else webSrv.reply("Mute off\n");
+    if(cmd == "mute"){              mute(); // at this time _f_mute has not changed
+                                    if(_f_mute) webSrv.reply("Mute off\n");
+                                    else webSrv.reply("Mute on\n");
                                     return;}
 
     if(cmd == "toneha"){            pref.putUShort("toneha",(param.toInt()));                             // vs1053 tone
