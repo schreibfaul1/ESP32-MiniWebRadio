fd49122af78815964a5c1aab161feb8a8f06d7e1
schreibfaul1 <schreibfaul1@t-online.de>
2022-04-12 20:23:03 +0200

    ESP32 Arduino V2.0.2

--- src/audiotask.cpp
+++ src/audiotask.cpp
@@ -123,7 +123,7 @@ void audioInit() {
     xTaskCreatePinnedToCore(
         audioTask,             /* Function to implement the task */
         "audioplay",           /* Name of the task */
-        5000,                  /* Stack size in words */
+        7000,                  /* Stack size in words */
         NULL,                  /* Task input parameter */
         AUDIOTASK_PRIO,        /* Priority of the task */
         NULL,                  /* Task handle. */
