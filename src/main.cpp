baa72fe4b47c682b419224bcd04326ae69878fa2
schreibfaul1 <schreibfaul1@t-online.de>
2023-08-15 14:46:09 +0200

    fix artifacts #278

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
     MiniWebRadio -- Webradio receiver for ESP32
 
     first release on 03/2017                                                                                                       */String Version="\
-    Version 2.8.4 Aug 13/2023                                                                                         ";
+    Version 2.8.4a Aug 13/2023                                                                                         ";
 
 /*  2.8" color display (320x240px) with controller ILI9341 or HX8347D (SPI) or
     3.5" color display (480x320px) wiht controller ILI9486 or ILI9488 (SPI)
@@ -2287,14 +2287,18 @@ void changeState(int state){
                 _f_semaphore = false;
                 _f_alarm = false;
             }
-            if(_state != CLOCKico) clearDigits();
-            clearAlarmDaysBar();
-            clearButtonBar();
+            if(_state == CLOCKico){
+                clearButtonBar();
+            }
+            else{ // != CLOCKico
+                clearLogoAndStationname();
+                clearTitle();
+                showFooter();
+                showHeadlineVolume();
+                showHeadlineTime();
+            }
             _state = CLOCK;
             showHeadlineItem(CLOCK);
-            showHeadlineVolume();
-            showHeadlineTime();
-            showFooter();
             display_time(true);
             break;
         }
