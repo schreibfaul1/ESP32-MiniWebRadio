9e0273f3fb5f03901f47062fa54c18cc7047d52b
schreibfaul1 <schreibfaul1@t-online.de>
2023-11-26 06:59:04 +0100

    little fix in endsWith()

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
     MiniWebRadio -- Webradio receiver for ESP32
 
     first release on 03/2017                                                                                                       */String Version="\
-    Version 3.00 Nov 21/2023                                                                                         ";
+    Version 3.00a Nov 26/2023                                                                                         ";
 
 /*  2.8" color display (320x240px) with controller ILI9341 or HX8347D (SPI) or
     3.5" color display (480x320px) wiht controller ILI9486 or ILI9488 (SPI)
@@ -2049,10 +2049,11 @@ bool startsWith(const char* base, const char* searchString) {
         if(c != *base++) return false;
     return true;
 }
-bool endsWith(const char* base, const char* searchString) {
-    int32_t         slen = strlen(searchString) - 1;
-    const char* p = base + strlen(base) - 1;
-    while(p > base && isspace(*p)) p--; // rtrim
+bool endsWith(const char *base, const char *searchString) {
+    int32_t slen = strlen(searchString);
+    if(slen == 0) return false;
+    const char *p = base + strlen(base);
+//  while(p > base && isspace(*p)) p--;  // rtrim
     p -= slen;
     if(p < base) return false;
     return (strncmp(p, searchString, slen) == 0);
