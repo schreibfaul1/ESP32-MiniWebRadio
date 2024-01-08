072f6559be6a65de759c0ad28eb74690497ba168
schreibfaul1 <schreibfaul1@t-online.de>
2024-01-08 16:49:52 +0100

    KCX_BT_EMITTER impl. Part 1a

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
     MiniWebRadio -- Webradio receiver for ESP32
 
     first release on 03/2017                                                                                                       */String Version="\
-    Version 3.00f Jan 04/2024                                                                                         ";
+    Version 3.00g Jan 07/2024                                                                                         ";
 
 /*  2.8" color display (320x240px) with controller ILI9341 or HX8347D (SPI) or
     3.5" color display (480x320px) wiht controller ILI9486 or ILI9488 (SPI)
