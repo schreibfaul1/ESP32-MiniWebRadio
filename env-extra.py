50365a2e04adf46b6d7938ff77d86bc44726a51f
Wolle <schreibfaul1@t-online.de>
2024-03-05 15:31:25 +0100

    Update env-extra.py

--- env-extra.py
+++ env-extra.py
@@ -1,18 +1,18 @@
 #pylint: disable = I,E,R,W,C,F
 from os.path import isfile
 Import("env")  # type: ignore
-assert isfile(".env")
-try:
-    f = open(".env", "r")
-    lines = f.readlines()
-    envs = []
-    for line in lines:
-        line.strip()
-        if line.startswith('#'):
-            continue
-        envs.append("-D{}".format(line.strip()))
-    env.Append(BUILD_FLAGS=envs) # type: ignore
-except IOError:
-    print("File .env not accessible",)
-finally:
-    f.close()
+if isfile(".env"):
+    try:
+        f = open(".env", "r")
+        lines = f.readlines()
+        envs = []
+        for line in lines:
+            line.strip()
+            if line.startswith('#'):
+                continue
+            envs.append("-D{}".format(line.strip()))
+        env.Append(BUILD_FLAGS=envs) # type: ignore
+    except IOError:
+        print("File .env not accessible",)
+    finally:
+        f.close()
