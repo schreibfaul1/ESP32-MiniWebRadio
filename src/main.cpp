4ead2c965e916b280cb348c247e25b6b72ff9163
schreibfaul1 <schreibfaul1@t-online.de>
2024-05-21 16:17:21 +0200

    change Version to V3.1

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
     MiniWebRadio -- Webradio receiver for ESP32
 
     first release on 03/2017                                                                                                      */String Version ="\
-    Version 3.01h  May 01/2024                                                                                                                       ";
+    Version 3.1   May 21/2024                                                                                                                       ";
 
 /*  2.8" color display (320x240px) with controller ILI9341 or HX8347D (SPI) or
     3.5" color display (480x320px) wiht controller ILI9486 or ILI9488 (SPI)
