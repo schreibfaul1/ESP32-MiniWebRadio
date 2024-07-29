92e6e62a759402cdb9f46f6cfc10b9817b876b35
schreibfaul1 <schreibfaul1@t-online.de>
2024-07-29 21:49:37 +0200

    New Version on Audiokit-A1S ES8388 #472 make constructor public

--- lib/es8388/ES8388.h
+++ lib/es8388/ES8388.h
@@ -5,13 +5,14 @@
 #include <inttypes.h>
 
 class ES8388{
+public:
     ES8388(TwoWire  *TwoWireInstance = &Wire);  // Constructor.
 
     bool write_reg(uint8_t slave_add, uint8_t reg_add, uint8_t data);
     bool read_reg(uint8_t slave_add, uint8_t reg_add, uint8_t &data);
     bool identify(int32_t sda, int32_t scl, uint32_t frequency);
 
-public:
+
     bool begin(int32_t sda = -1, int32_t scl = -1, uint32_t frequency = 400000U);
 
     enum ES8388_OUT{
