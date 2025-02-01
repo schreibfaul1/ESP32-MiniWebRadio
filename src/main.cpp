63ef7f74807fd5b916bd6382237949adffb2bdf8
schreibfaul1 <schreibfaul1@t-online.de>
2025-02-01 15:45:31 +0100

    typo TP_CS

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
     MiniWebRadio -- Webradio receiver for ESP32
 
     first release on 03/2017                                                                                                      */char Version[] ="\
-    Version 3.6.1i  - Jan 17/2025                                                                                                                   ";
+    Version 3.6.1j  - Feb 01/2025                                                                                                                   ";
 
 /*  2.8" color display (320x240px) with controller ILI9341 or HX8347D (SPI) or
     3.5" color display (480x320px) with controller ILI9486 or ILI9488 (SPI)
@@ -222,7 +222,7 @@ hp_BH1750           BH1750(&i2cBusOne);     // create the sensor
 
 #if TFT_CONTROLLER < 7 // ⏹⏹⏹⏹
 TFT_SPI     tft(SPI1, TFT_CS);
-TP_XPT2046  tp(SPI1, TFT_CS);
+TP_XPT2046  tp(SPI1, TP_CS);
 #else
 TFT_RGB     tft;
 TP_GT911    tp(&i2cBusOne);
