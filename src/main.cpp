f174f18fa667a657ffd63736fb6713540c38e880
schreibfaul1 <schreibfaul1@t-online.de>
2025-02-09 23:51:27 +0100

    doublr tft,begin() removed

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
     MiniWebRadio -- Webradio receiver for ESP32
 
     first release on 03/2017                                                                                                      */char Version[] ="\
-    Version 3.7-rc1.c   - Feb 07/2025                                                                                                                   ";
+    Version 3.7-rc1.d   - Feb 09/2025                                                                                                                   ";
 
 /*  2.8" color display (320x240px) with controller ILI9341 or HX8347D (SPI) or
     3.5" color display (480x320px) with controller ILI9486 or ILI9488 (SPI)
@@ -1457,12 +1457,6 @@ void setup() {
     else                   strcpy(_prefix, "m/");
 
     pref.begin("Pref", false);         // instance of preferences from AccessPoint (SSID, PW ...)
-
-#if CONFIG_IDF_TARGET_ESP32
-    tft.begin(TFT_CS, TFT_DC, VSPI, TFT_MOSI, TFT_MISO, TFT_SCK); // Init TFT interface ESP32
-#else
-    tft.begin(TFT_DC); // Init TFT interface ESP32S3
-#endif
 
 #if TFT_CONTROLLER < 7
     SPI1.begin(TFT_SCK, TFT_MISO, TFT_MOSI, -1); // SPI1 for TFT
