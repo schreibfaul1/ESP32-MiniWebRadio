7d0b17badb495fa87196588463d3976f866b9979
schreibfaul1 <schreibfaul1@t-online.de>
2025-03-06 16:03:11 +0100

    reduce jitter in rgb dma

--- src/common.h
+++ src/common.h
@@ -6,7 +6,7 @@
 // clang-format off
 #define _SSID                   "mySSID"                        // Your WiFi credentials here
 #define _PW                     "myWiFiPassword"                // Or in textfile on SD-card
-#define TFT_CONTROLLER          5                               // (0)ILI9341, (3)ILI9486, (4)ILI9488, (5)ST7796, (7) RGB display
+#define TFT_CONTROLLER          7                               // (0)ILI9341, (3)ILI9486, (4)ILI9488, (5)ST7796, (7) RGB display
 #define DISPLAY_INVERSION       0                               // only SPI displays, (0) off (1) on
 #define TFT_ROTATION            1                               // only SPI displays, 1 or 3 (landscape)
 #define TFT_FREQUENCY           40000000                        // only SPI displays, 80000000, 40000000, 27000000, 20000000, 10000000
@@ -87,44 +87,45 @@
 #endif
 
 #if TFT_CONTROLLER == 7 // RGB display
-const TFT_RGB::Pins RGB_PINS = {
-    .b0 = 15,
-    .b1 = 7,
-    .b2 = 6,
-    .b3 = 5,
-    .b4 = 4,
-    .g0 = 9,
-    .g1 = 46,
-    .g2 = 3,
-    .g3 = 8,
-    .g4 = 16,
-    .g5 = 1,
-    .r0 = 14,
-    .r1 = 21,
-    .r2 = 47,
-    .r3 = 48,
-    .r4 = 45,
-    .hsync = 39,
-    .vsync = 40,
-    .de = 41,
-    .pclk = 42,
-    .bl = 2
+
+const TFT_RGB::Pins RGB_PINS = { // WAVESHARE
+    .b0 = 14,
+    .b1 = 38,
+    .b2 = 18,
+    .b3 = 17,
+    .b4 = 10,
+    .g0 = 39,
+    .g1 = 0,
+    .g2 = 45,
+    .g3 = 48,
+    .g4 = 47,
+    .g5 = 21,
+    .r0 = 1,
+    .r1 = 2,
+    .r2 = 42,
+    .r3 = 41,
+    .r4 = 40,
+    .hsync = 46,
+    .vsync = 3,
+    .de = 5,
+    .pclk = 7,
+    .bl = -1
 };
 
 const TFT_RGB::Timing RGB_TIMING = {
     .h_res = 800,
     .v_res = 480,
-    .pixel_clock_hz = 10000000,
-    .hsync_pulse_width = 30,
-    .hsync_back_porch = 16,
-    .hsync_front_porch = 210,
-    .vsync_pulse_width = 13,
-    .vsync_back_porch = 10,
-    .vsync_front_porch = 22
+    .pixel_clock_hz = 12000000,
+    .hsync_pulse_width = 1,
+    .hsync_back_porch = 1,
+    .hsync_front_porch = 1,
+    .vsync_pulse_width = 4,
+    .vsync_back_porch = 4,
+    .vsync_front_porch = 4
 };
 
-#define TP_SDA 19
-#define TP_SCL 20
+#define TP_SDA 8
+#define TP_SCL 9
 #define TP_IRQ -1
 
 #define SD_MMC_CMD         11
@@ -132,24 +133,24 @@ const TFT_RGB::Timing RGB_TIMING = {
 #define SD_MMC_D0          13
 
 #define I2C_MASTER_FREQ_HZ 400000 // 400 kHz I2C-frequency
-#define GT911_I2C_ADDRESS  0x5D   // default I2C-address of GT911
+#define GT911_I2C_ADDRESS  0x14 //0x5D   // default I2C-address of GT911
 
-#define I2S_DOUT           17
-#define I2S_BCLK           0
-#define I2S_LRC            18
-#define I2S_MCLK           -1  // important!
+#define I2S_DOUT           19
+#define I2S_BCLK           20
+#define I2S_LRC            15
+#define I2S_MCLK           -1  // important, don't change!
 
-#define IR_PIN             38  // IR Receiver (if available)
+#define IR_PIN              6  // IR Receiver (if available)
 #define BT_EMITTER_RX      -1  // must be -1, not enough pins
 #define BT_EMITTER_TX      -1  // must be -1, not enough pins
 #define BT_EMITTER_LINK    -1  // must be -1, not enough pins
 #define BT_EMITTER_MODE    -1  // must be -1, not enough pins
 #define BT_EMITTER_CONNECT -1  // must be -1, not enough pins
 
-#define TFT_BL              2  // same as RGB_PINS.bl
+#define TFT_BL             -1  // same as RGB_PINS.bl
 
-#define I2C_SDA            19  // I2C line, same as dala line for capacitive touchpad  (-1 if not used)
-#define I2C_SCL            20  // I2C line, same as clock line for capacitive touchpad (-1 if not used)
+#define I2C_SDA            -1  // I2C line, same as dala line for capacitive touchpad  (-1 if not used)
+#define I2C_SCL            -1  // I2C line, same as clock line for capacitive touchpad (-1 if not used)
 
 
 #endif
