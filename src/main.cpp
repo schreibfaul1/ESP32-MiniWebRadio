80746a2c22512946382d9d3cb67f82b463f59e34
schreibfaul1 <schreibfaul1@t-online.de>
2026-08-24 07:15:57 +0200

    fix chunked in dlna

--- src/main.cpp
+++ src/main.cpp
@@ -3649,8 +3649,7 @@ void on_dlna_client(const DLNA_Client::msg_s& msg) {
                              "{}",
                              item.title, item.duration);
                 }
-            }
-            if (item.childCount) {
+            } else if (item.childCount) {
                 printfln(s_tag.dlna_server, "title " ANSI_ESC_YELLOW "{}" ANSI_ESC_RESET ", childCount " ANSI_ESC_CYAN "{}", item.title, item.childCount);
             } else {
                 printfln(s_tag.dlna_server, "title " ANSI_ESC_YELLOW "{}" ANSI_ESC_RESET ", childCount " ANSI_ESC_CYAN "{}", item.title, 0);
